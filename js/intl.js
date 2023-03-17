060ad778d10faec513f80e69b19406eb803dd298
Gonza <gonzalodutrasilva@gmail.com>
2023-03-17 00:15:06 -0300

    first

---
+++ js/intl.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", async (event) => {
+    const lang = window.location.search.includes("lang=es") ? "es" : "en";
+    const translations = await (await fetch(`../languages/${lang}.json`)).json();
+
+
+    const elements = document.querySelectorAll("[data-intl]");
+
+    for (const element of elements) {
+        const key = element.dataset.intl;
+        if (translations.hasOwnProperty(key)) {
+            const translation = translations[key];
+            element.innerText = translation;
+        }
+    }
+});

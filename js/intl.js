4ca59b8e3bc5da7e62c3fede7969579357814316
Gabriel Castiglioni <gabdsgs@hotmail.com>
2023-03-18 00:44:33 -0700

    Update intl.js

--- js/intl.js
+++ js/intl.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", async (event) => {
     const lang = window.location.search.includes("lang=es") ? "es" : "en";
-    const translations = await (await fetch(`../languages/${lang}.json`)).json();
+    const translations = await (await fetch(`./languages/${lang}.json`)).json();
 
 
     const elements = document.querySelectorAll("[data-intl]");
